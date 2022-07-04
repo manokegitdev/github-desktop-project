70f838aba9dc4fd23fb6444a0fdb5d1e384754a0
manokegitdev <108441144+manokegitdev@users.noreply.github.com>
2022-07-04 11:03:04 +0700

    Edit deposit
    Edit deposit.php and database local

--- application/views/home/deposit.php
+++ application/views/home/deposit.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="<?= site_url(); ?>public/assets/fonts/fontawesome-all.min.css">
 
-    <link rel="stylesheet" href="<?= site_url('public/assets/fonts/deposit_style.css'); ?>" />
+    <link rel="stylesheet" href="<?= site_url('public/assets/css/deposit_style.css'); ?>" />
     
 </head>
 
